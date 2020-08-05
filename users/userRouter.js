49b2e7fe6c578f6874ff3cada2e8c90a7240d78b
Natalia <nataliamirandac@hotmail.com>
2020-08-05 16:08:17 -0400

    DELETE and UPDATE user - works / Missing POST :id/posts

--- users/userRouter.js
+++ users/userRouter.js
@@ -15,8 +15,17 @@ router.post("/", validateUser("name"), (req, res) => {
     });
 });
 
-router.post("/:id/posts", (req, res) => {
+router.post("/:id/posts", validatePost("text"), (req, res) => {
   // do your magic!
+  userDb
+    .insert(req.body)
+    .then((post) => {
+      res.status(201).json(post);
+    })
+    .catch((error) => {
+      console.log("POST/isert - user - catch error:", error);
+      res.status(500).json({ error: "catch error POST /:id/posts" });
+    });
 });
 
 router.get("/", (req, res) => {
@@ -39,14 +48,45 @@ router.get("/:id", validateUserId, (req, res) => {
 
 router.get("/:id/posts", (req, res) => {
   // do your magic!
+  const { id } = req.params;
+  const user = req.body;
+  userDb.getUserPosts(id).then((item) => {
+    if (item.id === user.id) {
+      res.status(200).json(item);
+    } else {
+      res.status(500).json({ message: "catch error user getUserPosts" });
+    }
+  });
 });
 
 router.delete("/:id", (req, res) => {
   // do your magic!
+  const { id } = req.params;
+  // const user = req.body;
+
+  userDb.remove(id).then((item) => {
+    if (item == item > 0) {
+      res.status(200).json({ message: `The user ${id} has been deleted` });
+    } else {
+      res.status(500).json({
+        message: "Error removing the user",
+      });
+    }
+  });
 });
 
 router.put("/:id", (req, res) => {
   // do your magic!
+  const { id } = req.params;
+  const user = req.body;
+
+  userDb.update(id, user).then((item) => {
+    if ((item = id)) {
+      res.status(201).json({ ...user });
+    } else {
+      res.status(500).json({ error: "The post could not be updated" });
+    }
+  });
 });
 
 //custom middleware
@@ -77,8 +117,18 @@ function validateUser(prop) {
   };
 }
 
-function validatePost(req, res, next) {
+function validatePost(prop) {
   // do your magic!
+  return function (req, res, next) {
+    // do your magic!
+    if (req.body[prop]) {
+      next();
+    } else {
+      res.status(400).json({
+        message: `from middleware: Please provide the ${prop}`,
+      });
+    }
+  };
 }
 
 module.exports = router;
