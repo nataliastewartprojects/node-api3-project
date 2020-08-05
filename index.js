72fcc43e25ddcb742f69a2a704f8984132e55746
Natalia <nataliamirandac@hotmail.com>
2020-08-05 15:15:37 -0400

    Middleware Logger works - GET users works

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // code away!
+const express = require("express");
+const server = express();
+const usersRouter = require("./users/userRouter.js");
+
+//middlewares
+server.use(express.json());
+server.use(logger);
+
+//Routers
+server.use("/api/users", usersRouter);
+
+//testing server
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
+});
+
+//logger middleware
+function logger(req, res, next) {
+  console.log(
+    `[timestamp: ${new Date().toISOString()}] method: ${req.method} to url: ${
+      req.url
+    } 
+    )}`
+  );
+
+  next();
+}
+
+server.listen(8000, () => {
+  console.log("\n*** Server Running on http://localhost:8000 ***\n");
+});
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,32 +1,42 @@
-const express = require('express');
+const express = require("express");
+const userDb = require("./userDb.js");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
+  // do your magic!
+  userDb
+    .get(req.body)
+    .then((users) => {
+      res.status(200).json({ headers: req.headers, users });
+    })
+    .catch((error) => {
+      console.log("GET users catch error:", error);
+      res.status(500).json({ error: "catch error GET users" });
+    });
+});
+
+router.get("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id/posts', (req, res) => {
+router.delete("/:id", (req, res) => {
   // do your magic!
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
